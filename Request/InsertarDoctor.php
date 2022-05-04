d490811c468305dc60c316b934e363e8bcbe89fd
sergio-torres-17 <50086305+sergio-torres-17@users.noreply.github.com>
2022-05-04 00:29:11 -0500

    Alta de medicos
    Modulo de registro de médicos

--- Request/InsertarDoctor.php
+++ Request/InsertarDoctor.php
@@ -3,26 +3,30 @@ include ('MultiTool.php');
 include ('Archivos.php');
 include ('Consultas.php');
 //Constantes
-$rutaBaseArchivos = $_SERVER['DOCUMENT_ROOT'] .'/hopeappbackend/archivos/';
+$rutaBaseArchivos = $_SERVER['DOCUMENT_ROOT'] .'/HopeAppBackend/Request/archivos/';
 $multiToolAux = new MultiTool();
 $archivos = new ControArchivos();
 $consultas= new Consultas();
 //Variables de peticion 
-$nombre = $_GET['nombre'];
-$apellidos = $_GET['apellidos'];
-$edad = $_GET['edad'];
-$email = $_GET['email'];
-$pass = $_GET['pass'];
-$cedula = $_GET['cedula'];
-$especialidad = $_GET['especialidad'];
-$estudios = $_GET['estudios'];
-$historial = $_GET['historial'];
-$certificado = $_GET['certificado'];
+$nombre = $_POST['nombre'];
+$apellidos = $_POST['apellidos'];
+$edad = $_POST['edad'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+$cedula = $_POST['cedula'];
+$especialidad = $_POST['especialidad'];
+$estudios = $_POST['estudios'];
+$historial = $_POST['historial'];
+$certificado = $_POST['certificado'];
+$imgPerfil = $_POST['imgPerfil'];
 $nombreCarpetaArchivos = $rutaBaseArchivos .  $multiToolAux->GenerarNombreCarpetaUsuario($nombre, $apellidos). '/';
 $archivos->CrearDirectorio($nombreCarpetaArchivos);
 $archivos->CrearDirectorio($nombreCarpetaArchivos.'Fotos/');
-$nombreFotoPerfil = $multiToolAux->GenerarNombreArchivoFotoPerfil($nombre,$apellidos);
+$nombreFotoPerfil = $nombreCarpetaArchivos.'Fotos/' . $multiToolAux->GenerarNombreArchivoFotoPerfil($nombre,$apellidos) . '.png';
 $certificado = $nombreCarpetaArchivos.'Fotos/' . $certificado;
 $historial = $nombreCarpetaArchivos.'Fotos/' . $historial;
+//Guardado de imagenes
+file_put_contents($nombreFotoPerfil, base64_decode($imgPerfil));
+
 echo $consultas->insertarDoctor($nombre,$apellidos,$edad,$email,$pass,$cedula,$especialidad,$estudios,$historial,$certificado,$nombreFotoPerfil);
 ?>
--- Request/LoginDoctor.php
+++ Request/LoginDoctor.php
@@ -1,7 +1,7 @@
 <?php
 include ('Consultas.php');
 $obj = new Consultas();
-$usr = $_GET['usr'];
-$pwd = $_GET['pwd'];
+$usr = $_POST['usr'];
+$pwd = $_POST['pwd'];
 echo $obj->LoginDoctores($usr, $pwd);
 ?>
